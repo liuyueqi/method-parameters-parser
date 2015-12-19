99b37ab30e625c8dc8ddb6815349ae56e97a02fb
liuyueqi <c4_lyq@126.com>
2015-12-19 11:09:04 +0800

    Create MethodParametersParser.java

---
+++ src/main/java/com/liuyueqi/method/parameters/parser/MethodParametersParser.java
@@ -0,0 +1,14 @@
+package com.liuyueqi.method.parameters.parser;
+
+import java.lang.reflect.Method;
+
+public class MethodParametersParser {
+
+    private Method method;
+    
+    public MethodParametersParser(Method method) {
+        this.method = method;
+    }
+    
+    
+}
